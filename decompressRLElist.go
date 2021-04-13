2e8fa3d1f54826e2000589a6754bd6850401b7b7
Topexan <54759948+Topexan@users.noreply.github.com>
2021-04-13 11:53:24 +0600

    Decompress Run-Length Encoded List, Leetcode

---
+++ decompressRLElist.go
@@ -0,0 +1,11 @@
+package Leetcode
+
+func decompressRLElist(nums []int) []int {
+	var res []int
+	for i := 0; i < len(nums); i = i + 2 {
+		for j := 0; j < nums[i]; j++ {
+			res = append(res, nums[i+1])
+		}
+	}
+	return res
+}
