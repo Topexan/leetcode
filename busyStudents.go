e623b398da2c3be1195c01abce9d24eb5106f21e
Topexan <54759948+Topexan@users.noreply.github.com>
2021-05-10 00:46:19 +0600

    busy students, Leetcode problem

---
+++ busyStudents.go
@@ -0,0 +1,11 @@
+package leetcode
+
+func busyStudent(startTime []int, endTime []int, queryTime int) int {
+    res := 0
+    for i := range startTime {
+        if startTime[i] <= queryTime && endTime[i] >= queryTime {
+            res++
+        }
+    }
+    return res
+}
