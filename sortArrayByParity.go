b525a5a6f428b1c20ac06da9b27291b8c9bef237
Topexan <54759948+Topexan@users.noreply.github.com>
2021-05-13 09:37:28 +0600

    sort array by parity, Leetcode problem

---
+++ sortArrayByParity.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func sortArrayByParity(A []int) []int {
+	var res []int
+	for i := range A {
+		if A[i]%2 == 0 {
+			res = append(res, A[i])
+		}
+	}
+	for i := range A {
+		if A[i]%2 != 0 {
+			res = append(res, A[i])
+		}
+	}
+	return res
+}
